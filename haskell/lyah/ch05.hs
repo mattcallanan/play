8aed8395408831caa17d0a8452a06ef6b6244578
Matt Callanan <matthew_callanan@yahoo.com.au>
2011-12-20 20:07:26 +1000

    LYAH Chapter 5 Recursion

--- haskell/lyah/ch05.hs
+++ haskell/lyah/ch05.hs
@@ -10,7 +10,67 @@ dubFac n
 
 
 
---log2 n = 
+
+dubFac' x = dubFacTail x 1
+    where dubFacTail n a
+              | n < 2 = a
+              | otherwise = dubFacTail (n-2) (n*a)
+
+doubleF n = doubleF' n 1
+doubleF' 0 a = a
+doubleF' 1 a = a
+doubleF' n a = doubleF' (n-2) (n*a)
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+log2 n 
+    | n < 2 = 0
+    | otherwise = 1 + log2 (n/2)
+
+--log2' n = log2T n 
+--log2T 1 a = a
+--log2T n a = log2T (n/2) (a+1)
+    
+    
+log2' = truncate . sqrt
+
+
+
+
+
+
+
+--hanoiA n x y z | trace ("n:" ++ show n ++ ", x:" ++ show x ++ ", y:" ++ show y ++ ", z:" ++ show z) False = undefined
+hanoiA 0 _ _ _ = []
+--hanoiA n x y z = hanoiA (n-1) x z y ++ [(x,y)] ++ hanoiA (n-1) z y x
+hanoiA n x y z = hanoiA (n-1) x z y ++ [(x,z)] ++ hanoiA (n-1) y x z
+
+applyMoves ((from,to):nextMoves) pegs | trace ("(from:" ++ show from ++ ", to:" ++ show to ++ ") pegs:" ++ show pegs) False = undefined
+applyMoves [] pegs = pegs
+applyMoves ((1,2):nextMoves) pegs = applyMoves nextMoves [tail (pegs!!0),         head (pegs!!0):pegs!!1, pegs!!2]
+applyMoves ((1,3):nextMoves) pegs = applyMoves nextMoves [tail (pegs!!0),         pegs!!1,                head (pegs!!0):pegs!!2]
+applyMoves ((2,1):nextMoves) pegs = applyMoves nextMoves [head (pegs!!1):pegs!!0, tail (pegs!!1),         pegs!!2]
+applyMoves ((2,3):nextMoves) pegs = applyMoves nextMoves [pegs!!0,                tail (pegs!!1),         head (pegs!!1):pegs!!2]
+applyMoves ((3,1):nextMoves) pegs = applyMoves nextMoves [head (pegs!!2):pegs!!0, pegs!!1,                tail (pegs!!2)]
+applyMoves ((3,2):nextMoves) pegs = applyMoves nextMoves [pegs!!0,                head (pegs!!2):pegs!!1, tail (pegs!!2)]
+applyMoves (dodgyMove:nextMoves) pegs = error "Invalid moves: "
 
 
 --hanoi :: Num a => a -> [[a]] -> [[a]]
@@ -33,6 +93,22 @@ step3 disk (source:spare:[dest]) = [source', spare', dest']
 
 moveTopDisk a (source:spare:[dest]) | trace ((replicate a ' ') ++ "moveT " ++ show a ++ " source:" ++ show source ++ ", spare:" ++ show spare ++ ", dest:" ++ show dest) False = undefined
 moveTopDisk disk (source:spare:[dest]) = [tail source,spare,head source:dest]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
               
 --testHanoi = (hanoi [[1..4],[],[]]) `assertEquals` [[],[],[1..4]] 
