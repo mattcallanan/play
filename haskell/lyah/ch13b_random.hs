a50aca12d8a1598f167a11cd0f9293293da732d9
Matt Callanan <matthew_callanan@yahoo.com.au>
2012-04-15 17:50:36 +1000

    LYAH Chapter 14

--- haskell/lyah/ch13b_random.hs
+++ haskell/lyah/ch13b_random.hs
@@ -1,59 +1,12 @@
+import System.Random
 import Control.Monad.State
 
-{--
-Exercise 1
-Given the WriterMonad and the StateMonad can both store some kind of state,
-compare and contrast the two monads and explain where you would use one over the other.
---}
+randomSt :: (RandomGen g, Random a) => State g a
+randomSt = state random
 
-{-- Exercise 2 --}
-data Queue a = EmptyQ | Q[a] deriving (Show, Eq)
-
-colours = ["red", "green", "blue", "yellow", "magenta", "teal", "burgundy", "gray"]
-numbers = [1..100]
-
-testColours = (((runState stateColours) $ Q(colours)) == ((), Q["red", "green", "magenta"]))    
-testNumbers = (((runState stateNumbers) $ Q(numbers)) == ((), Q[404]))
-
-stateColours :: State (Queue String) ()
-stateColours = do
-  enqueue "maroon"
-  enqueue "scarlet"
-  filterQ ('l' `notElem`)
-  dequeue
-  filterQ('e' `elem`)
-
-stateNumbers :: State (Queue Int) ()
-stateNumbers  = do
-  dequeue
-  dequeue
-  filterQ (\x -> x >= 40 && x <= 50)
-  enqueue 101
-  filterQ (\x -> x `mod` 2 == 0)
-  dequeue
-  dequeue
-  filterQ (\x -> x `mod` 2 /= 0)
-  enqueue 404
-
---write implementations for the following:
-
-enqueue :: a -> State (Queue a) ()  --adds an element to the end of the Queue
-enqueue x = state $ \(Q xs) -> ((), (Q (x:xs)))
-
-dequeue :: State (Queue a) ()  --removes the last element
-dequeue = state $ \(Q (_:xs)) -> ((), Q(xs))
-
-filterQ ::(Eq a) => (a -> Bool) -> State (Queue a) () --returns elements of the Queue that satisfy the predicate
-filterQ p =  state $ \(Q xs) -> ((), Q(filter p xs))
-
-x = do
-      enqueue "maroon"
-      enqueue "scarlet"
-
-{-
-Exercise 3
-What would we gain/lose if we implemented the above exercise without the State Monad?
-
-Gain: clarity
-Lose: ability to use 'do' notation
--}
+threeCoins :: State StdGen (Bool, Bool, Bool)
+threeCoins = do
+    a <- randomSt
+    b <- randomSt
+    c <- randomSt
+    return (a, b, c)
---
+++ haskell/lyah/ch14_old.hs
@@ -0,0 +1,136 @@
+{-
+### Question 1
+From the example in the book:
+    let newFocus = modify (\_ -> 'P') (goRight (goLeft (freeTree,[])))  
+What are the elements as in freeTree as it goes through the left and right branches and what element is replaced in the tree?
+-}
+-- ['O', 'Y']
+-- 'Y' gets replace with 'P'
+
+{-
+### Question 2
+
+Implement a text editor that:
+
+* Moves up a line,
+* Moves down a line,
+* Inserts a line,
+* Deletes a line,
+* Replaces a line,
+* Moves to the start of the document,
+* Moves to the end of the document.
+
+Make sure we get can't go passed the top or bottom of the document.
+-}
+
+{- Function signatures: -}
+
+type HEdit = ([String], [String])
+cursorUp :: HEdit -> HEdit
+cursorDown :: HEdit -> HEdit
+insertLine :: String -> HEdit -> HEdit
+deleteLine :: HEdit -> HEdit
+replaceLine :: String -> HEdit -> HEdit
+startDocument :: HEdit -> HEdit      -- "home" line
+endDocument :: HEdit -> HEdit        -- "end" line
+
+{- Tests: -}
+
+line1 = "first line"
+line2 = "second line"
+line3 = "third line"
+emptyDocument = ([], [])
+document = ([line1, line2, line3], [])
+cursorUpOk1 = cursorUp(document) == document
+cursorUpOk2 = cursorUp([line2, line3],[line1]) == document
+cursorDownOk1 = cursorDown(document) == ([line2, line3], [line1])
+cursorDownOk2 = cursorDown([], [line3, line2, line1]) == cursorDown([], [line3, line2, line1])
+startDocumentOk = startDocument(cursorDown(cursorDown(document))) == document
+endDocumentOk = endDocument(document) == ([], [line3, line2, line1])
+insertLineOk = insertLine line1 (insertLine line2 (insertLine line3 emptyDocument)) == document
+deleteLineOk1 = deleteLine document == ([line2, line3], [])
+deleteLineOk2 = deleteLine emptyDocument == emptyDocument
+deleteLineOk3 = deleteLine(endDocument(document)) == ([], [line2, line1])
+replaceLineOk1 = replaceLine "skink" document == (["skink", line2, line3], [])
+complicated = endDocument(replaceLine "george" (cursorUp(replaceLine "fred" (deleteLine(cursorDown(document)))))) == ([], ["fred", "george"])
+allTests = [cursorUpOk1, cursorUpOk2, cursorDownOk1, cursorDownOk2, startDocumentOk, endDocumentOk, insertLineOk,deleteLineOk1, deleteLineOk2, deleteLineOk3, replaceLineOk1, complicated]
+
+{- Answer -}
+
+cursorUp (xs, []) = (xs, [])
+cursorUp (xs, b:bs) = (b:xs, bs)
+cursorDown ([], bs) = ([], bs)
+cursorDown (x:xs, bs) = (xs, x:bs)
+insertLine l (xs, bs) = (l:xs, bs)
+deleteLine ([], bs) = ([], bs)  -- should this delete the last breadcrumb? I don't think so.
+deleteLine (_:xs, bs) = (xs, bs)
+replaceLine _ ([], bs) = ([], bs)
+replaceLine l (_:xs, bs) = (l:xs, bs)
+startDocument (xs, []) = (xs, [])
+startDocument z = startDocument $ cursorUp z
+endDocument ([], bs) = ([], bs)
+endDocument z = endDocument $ cursorDown z
+
+
+{-
+### Question 3
+
+A nexus is a tree that shares nodes (see page 3 of [Functional Pearl Trouble Shared is Trouble Halved](http://www.cs.ox.ac.uk/ralf.hinze/publications/HW03.pdf)).
+It splits the value of the node into two parts using the following function:
+
+isegs xs = [init xs, tail xs]
+isegs [1,2,3,4] = [ [1,2,3],[2,3,4] ]
+-}
+
+{- Write a program that simulates a nexus, using the following functions and data: -}
+
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show, Eq)
+data Crumb a = LeftCrumb a (Tree a) | RightCrumb a (Tree a) deriving Show
+type Breadcrumbs a = [Crumb a]
+type Zipper a = (Tree a, Breadcrumbs a)
+
+
+modify :: (a -> a) -> Zipper a -> Zipper a
+modify _ (Empty, bs) = (Empty, bs)
+modify f (Node x l r, bs) = (Node (f x) l r, bs)
+
+attach :: Tree a -> Zipper a -> Zipper a
+attach t (_, bs) = (t, bs)
+
+isegs xs = [init xs, tail xs]
+okIsegs = isegs [1,2,3,4] == [ [1,2,3],[2,3,4] ]
+
+--nexus :: Tree [Int]
+startNexus = Node [1,2,3,4] Empty Empty
+startNexusZipper = (startNexus, [])
+endNexus =
+    Node [1,2,3,4]
+        (Node [1,2,3]
+            (Node [1,2]
+                (Node [1] Empty Empty)
+                (Node [2] Empty Empty)
+            )
+            (Node [2,3]
+                (Node [2] Empty Empty)
+                (Node [3] Empty Empty)
+            )
+        )
+        (Node [2,3,4]
+            (Node [2,3]
+                (Node [2] Empty Empty)
+                (Node [3] Empty Empty)
+            )
+            (Node [3,4]
+                (Node [3] Empty Empty)
+                (Node [4] Empty Empty)
+            )
+        )
+
+createNexus :: [Int] -> [Int]
+-- createNexus takes a  list of Ints and returns a subtree of lists of Ints
+createNexus xs = xs
+
+
+{- Make this True: -}
+
+okBeginNexus = (fst (modify createNexus startNexusZipper)) == (endNexus)
